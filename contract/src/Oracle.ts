bff7bbd913fb76b50509643c19d37b80d5470e25
Korney Vasilchenko <starkkor@gmail.com>
2022-09-13 21:03:46 +0100

    Add oracle response typing

---
+++ contract/src/Oracle.ts
@@ -0,0 +1,20 @@
+/**
+ * Oracle response typing for:
+ * NEAR_ENV=mainnet near view priceoracle.near get_price_data
+ */
+
+export interface Price {
+  multiplier: string;
+  decimals: number;
+}
+
+export interface Asset {
+  asset_id: string;
+  price: Price;
+}
+
+export interface PricesResponse {
+  timestamp: string;
+  recency_duration_sec: number;
+  prices: Price[];
+}
