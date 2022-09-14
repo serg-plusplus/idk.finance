56f7670c397e2b10cec27c607b86e88d5e395507
pas1ko <git@serh11p.com>
2022-09-14 02:23:02 +0100

    Panel

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -8,6 +8,7 @@ import { SignInPrompt } from "./ui-components";
 import Chart from "./chart/Chart2";
 import { useIdkState } from "./idk-state";
 import Header from "./Header/Header";
+import Panel from "./Panel/Panel";
 
 const App: FC = () => {
   const { isSignedIn, wallet, chartData, state, latestRounds } = useIdkState();
@@ -26,54 +27,7 @@ const App: FC = () => {
             <Chart stock={chartData} width={1200} height={400} />
           )}
 
-          {state && latestRounds?.length && (
-            <Card css={{ p: "$6", marginTop: 16 }}>
-              <Card.Header>
-                {/* <Text size={50}>ѻ</Text> */}
-                <Grid.Container>
-                  <Grid xs={12}>
-                    <Text h2 css={{ lineHeight: "$xs" }}>
-                      Current round #{state.currentEpoch.toFixed()}
-                    </Text>
-                  </Grid>
-                  <Grid xs={12}>
-                    <Text css={{ color: "$accents8" }}>
-                      Total amount: {latestRounds[0].totalAmount}
-                    </Text>
-                  </Grid>
-                  <Grid xs={12}>
-                    <Text css={{ color: "$accents8" }}>
-                      Bears staked: {latestRounds[0].bearAmount}
-                    </Text>
-                  </Grid>
-                  <Grid xs={12}>
-                    <Text css={{ color: "$accents8" }}>
-                      Bulls staked: {latestRounds[0].bullAmount}
-                    </Text>
-                  </Grid>
-                  <Grid xs={12}>
-                    <Text css={{ color: "$accents8" }}>
-                      Close timestamp: {latestRounds[0].closeTimestamp}
-                    </Text>
-                  </Grid>
-                </Grid.Container>
-              </Card.Header>
-              <Card.Body css={{ py: "$2" }}>
-                <Text>
-                  Make beautiful websites regardless of your design experience.
-                </Text>
-              </Card.Body>
-              <Card.Footer>
-                <Link
-                  color="primary"
-                  target="_blank"
-                  href="https://github.com/nextui-org/nextui"
-                >
-                  Visit source code on GitHub.
-                </Link>
-              </Card.Footer>
-            </Card>
-          )}
+          <Panel />
         </Container>
       </main>
     );
