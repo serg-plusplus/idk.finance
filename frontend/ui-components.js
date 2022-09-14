9ef86f61b6029c567b823571ccf08234b9c1c8db
pas1ko <git@serh11p.com>
2022-09-14 01:05:32 +0100

    Improve UI; add logo

--- frontend/ui-components.js
+++ frontend/ui-components.js
@@ -1,8 +1,18 @@
-import React from 'react';
-import { Container, Button, Text, Link, Spacer, Modal, Row, Col, Checkbox } from '@nextui-org/react'
-import { Discovery, ChevronRight, Search } from './icons';
+import React from "react";
+import {
+  Container,
+  Button,
+  Text,
+  Link,
+  Spacer,
+  Modal,
+  Row,
+  Col,
+  Checkbox,
+} from "@nextui-org/react";
+import { Discovery, ChevronRight, Search } from "./icons";
 
-export function SignInPrompt({greeting, onClick}) {
+export function SignInPrompt({ onClick }) {
   const [visible, setVisible] = React.useState(false);
   const [selectedCheckbox, setSelectedCheckbox] = React.useState({
     first: false,
@@ -34,23 +44,26 @@ export function SignInPrompt({greeting, onClick}) {
           >
             Decentralized Betting Protocol
           </Text>
-          <Spacer y={0.5}/>
+          <Spacer y={0.5} />
           <Text size="$2xl">
-            <Link href="/" css={{display: "inline"}}>idk.finance</Link> is an automated prediction
-            market platform for tracking of and betting on future real-world events driven by
-            oracles and market incentives.
+            <Link href="/" css={{ display: "inline" }}>
+              idk.finance
+            </Link>{" "}
+            is an automated prediction market platform for tracking of and
+            betting on future real-world events driven by oracles and market
+            incentives.
           </Text>
-          <Spacer y={3}/>
+          <Spacer y={3} />
           <Row justify="center">
             <Button onClick={handler} color="gradient" className="button">
               <Discovery />
-              <Spacer x={0.5}/>
+              <Spacer x={0.5} />
               Launch idk.finance
             </Button>
-            <Spacer x={1}/>
+            <Spacer x={1} />
             <Button flat color="primary" className="button">
               <Search />
-              <Spacer x={0.5}/>
+              <Spacer x={0.5} />
               Learn more
             </Button>
           </Row>
@@ -66,15 +79,17 @@ export function SignInPrompt({greeting, onClick}) {
           <Col>
             <Text id="modal-title" size={18}>
               Welcome to{" "}
-              <Text b size={18} css={{
-                textGradient: "45deg, $blue600 -20%, $pink600 50%",
-              }}>
+              <Text
+                b
+                size={18}
+                css={{
+                  textGradient: "45deg, $blue600 -20%, $pink600 50%",
+                }}
+              >
                 idk.finance
               </Text>
             </Text>
-            <Text size={18}>
-              This Product is in beta.
-            </Text>
+            <Text size={18}>This Product is in beta.</Text>
           </Col>
         </Modal.Header>
         <Modal.Body>
@@ -83,40 +98,51 @@ export function SignInPrompt({greeting, onClick}) {
           </Text>
           <Checkbox
             isSelected={selectedCheckbox.first}
-            onChange={() => setSelectedCheckbox(prevState => ({
-              ...prevState,
-              first: !prevState.first
-            }))}
+            onChange={() =>
+              setSelectedCheckbox((prevState) => ({
+                ...prevState,
+                first: !prevState.first,
+              }))
+            }
           >
             <Text size={14}>I am over 21 years old.</Text>
           </Checkbox>
           <Checkbox
             isSelected={selectedCheckbox.second}
-            onChange={() => setSelectedCheckbox(prevState => ({
-              ...prevState,
-              second: !prevState.second
-            }))}
+            onChange={() =>
+              setSelectedCheckbox((prevState) => ({
+                ...prevState,
+                second: !prevState.second,
+              }))
+            }
           >
             <Text size={14}>
-              I understand that I am using this product at my own risk. Any losses incurred due to my actions are my own responsibility.
+              I understand that I am using this product at my own risk. Any
+              losses incurred due to my actions are my own responsibility.
             </Text>
           </Checkbox>
           <Checkbox
             isSelected={selectedCheckbox.third}
-            onChange={() => setSelectedCheckbox(prevState => ({
-              ...prevState,
-              third: !prevState.third
-            }))}
+            onChange={() =>
+              setSelectedCheckbox((prevState) => ({
+                ...prevState,
+                third: !prevState.third,
+              }))
+            }
           >
             <Text size={14}>
-              I understand that this product is still in beta. I am participating at my own risk.
+              I understand that this product is still in beta. I am
+              participating at my own risk.
             </Text>
           </Checkbox>
         </Modal.Body>
         <Modal.Footer>
           <Row justify="center">
             <Button
-              disabled={Object.values(selectedCheckbox).filter(item => !item).length >= 1}
+              disabled={
+                Object.values(selectedCheckbox).filter((item) => !item)
+                  .length >= 1
+              }
               onClick={() => {
                 closeHandler();
                 onClick();
@@ -124,7 +150,7 @@ export function SignInPrompt({greeting, onClick}) {
               className="button"
             >
               Continue
-              <Spacer x={0.5}/>
+              <Spacer x={0.5} />
               <ChevronRight />
             </Button>
           </Row>
@@ -134,9 +160,9 @@ export function SignInPrompt({greeting, onClick}) {
   );
 }
 
-export function SignOutButton({accountId, onClick}) {
+export function SignOutButton({ accountId, onClick }) {
   return (
-    <button style={{ float: 'right' }} onClick={onClick}>
+    <button style={{ float: "right" }} onClick={onClick}>
       Sign out {accountId}
     </button>
   );
@@ -146,20 +172,44 @@ export function EducationalText() {
   return (
     <>
       <p>
-        Look at that! A Hello World app! This greeting is stored on the NEAR blockchain. Check it out:
+        Look at that! A Hello World app! This greeting is stored on the NEAR
+        blockchain. Check it out:
       </p>
       <ol>
         <li>
-          Look in <code>frontend/App.js</code> - you'll see <code>getGreeting</code> and <code>setGreeting</code> being called on <code>contract</code>. What's this?
+          Look in <code>frontend/App.js</code> - you'll see{" "}
+          <code>getGreeting</code> and <code>setGreeting</code> being called on{" "}
+          <code>contract</code>. What's this?
         </li>
         <li>
-          Ultimately, this <code>contract</code> code is defined in <code>./contract</code> – this is the source code for your <a target="_blank" rel="noreferrer" href="https://docs.near.org/docs/develop/contracts/overview">smart contract</a>.</li>
+          Ultimately, this <code>contract</code> code is defined in{" "}
+          <code>./contract</code> – this is the source code for your{" "}
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://docs.near.org/docs/develop/contracts/overview"
+          >
+            smart contract
+          </a>
+          .
+        </li>
         <li>
-          When you run <code>npm run deploy</code>, the code in <code>./contract</code> gets deployed to the NEAR testnet. You can see how this happens by looking in <code>package.json</code>.</li>
+          When you run <code>npm run deploy</code>, the code in{" "}
+          <code>./contract</code> gets deployed to the NEAR testnet. You can see
+          how this happens by looking in <code>package.json</code>.
+        </li>
       </ol>
       <hr />
       <p>
-        To keep learning, check out <a target="_blank" rel="noreferrer" href="https://docs.near.org">the NEAR docs</a> or look through some <a target="_blank" rel="noreferrer" href="https://examples.near.org">example apps</a>.
+        To keep learning, check out{" "}
+        <a target="_blank" rel="noreferrer" href="https://docs.near.org">
+          the NEAR docs
+        </a>{" "}
+        or look through some{" "}
+        <a target="_blank" rel="noreferrer" href="https://examples.near.org">
+          example apps
+        </a>
+        .
       </p>
     </>
   );
